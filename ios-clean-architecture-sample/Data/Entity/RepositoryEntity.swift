063058aa614376c0be14a057abce76eb75064c67
KAGE <kagemiku@gmail.com>
2017-01-08 17:44:50 +0900

    Add RepositoryEntity class

---
+++ ios-clean-architecture-sample/Data/Entity/RepositoryEntity.swift
@@ -0,0 +1,14 @@
+//
+//  RepositoryEntity.swift
+//  ios-clean-architecture-sample
+//
+//  Created by KAGE on 1/8/17.
+//  Copyright © 2017 KAGE. All rights reserved.
+//
+
+import Foundation
+
+struct RepositoryEntity {
+    var id = 0
+    var full_name = ""
+}
