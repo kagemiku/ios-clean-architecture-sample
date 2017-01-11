11907dbe263113c316f6893e649b21cc42a7dd70
KAGE <kagemiku@gmail.com>
2017-01-12 00:41:13 +0900

    Add headerView to DetailVC

--- ios-clean-architecture-sample/Presentation/UI/ViewController/GitHubRepositoryDetailViewController.swift
+++ ios-clean-architecture-sample/Presentation/UI/ViewController/GitHubRepositoryDetailViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class GitHubRepositoryDetailViewController: UIViewController {
     fileprivate lazy var detailTableView: UITableView = self.createDetailTableView()
+    fileprivate lazy var headerView: GitHubRepositoryDetailHeaderView = self.createHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.addSubview(self.detailTableView) 
+        self.view.addSubview(self.detailTableView)
+        self.layoutHeaderView()
+        self.detailTableView.tableHeaderView = self.headerView
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
         self.layoutDetailTableView()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        self.detailTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,11 +39,24 @@ class GitHubRepositoryDetailViewController: UIViewController {
 
     private func createDetailTableView() -> UITableView {
         let tableView = UITableView(frame: CGRect.zero)
+
         return tableView
     }
 
+    private func createHeaderView() -> GitHubRepositoryDetailHeaderView {
+        let headerView = GitHubRepositoryDetailHeaderView(frame: CGRect.zero)
+        headerView.update(repositoryName: "text", watchersNumber: 0, starsNumber: 0, forksNumber: 0)
+
+        return headerView
+    }
+
     private func layoutDetailTableView() {
-        let frame = self.view.bounds
+        let frame = self.view.frame
         self.detailTableView.frame = frame
+    }
+
+    private func layoutHeaderView() {
+        let frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 100.0)
+        self.headerView.frame = frame
     }
 }
