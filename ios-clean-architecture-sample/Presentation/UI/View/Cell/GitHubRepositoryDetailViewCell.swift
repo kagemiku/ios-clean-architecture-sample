8e7ae39a61a68e6e449748681f579d1bd270fef9
KAGE <kagemiku@gmail.com>
2017-01-20 00:55:56 +0900

    Delete unnecessary codes

--- ios-clean-architecture-sample/Presentation/UI/View/Cell/GitHubRepositoryDetailViewCell.swift
+++ ios-clean-architecture-sample/Presentation/UI/View/Cell/GitHubRepositoryDetailViewCell.swift
@@ -11,17 +11,6 @@ import UIKit
 class GitHubRepositoryDetailViewCell: UITableViewCell {
     static let cellIdentifier = String(describing: GitHubRepositoryDetailViewCell.self)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
     func configure(text: String?, icon: UIImage?) {
         self.textLabel?.text = text
         self.imageView?.image = icon
