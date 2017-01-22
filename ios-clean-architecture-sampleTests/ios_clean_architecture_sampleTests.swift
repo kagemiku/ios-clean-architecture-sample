3d6d589b49c95214031f38c46432384cf2e3936d
KAGE <kagemiku@gmail.com>
2017-01-22 23:15:17 +0900

    Add unit test bundle

---
+++ ios-clean-architecture-sampleTests/ios_clean_architecture_sampleTests.swift
@@ -0,0 +1,35 @@
+//
+//  ios_clean_architecture_sampleTests.swift
+//  ios-clean-architecture-sampleTests
+//
+//  Created by KAGE on 1/22/17.
+//  Copyright © 2017 KAGE. All rights reserved.
+//
+
+import XCTest
+
+class ios_clean_architecture_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
